038b19163e348a2d51b0356733faea922d8c6c31
alex-yellow <hitys2000@gmail.com>
2023-10-08 15:21:25 +0300

    final

--- redux.js
+++ redux.js
@@ -1,13 +1,33 @@
-function counter(state=0, action){
-    if(action.type == 'INCREMENT'){
-        return state + 1;
-    }
-    else if(action.type == 'DECREMENT'){
-        return state - 1;
-    }
-    else{
-        return state;
+// Создаем редюсер
+function counter(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
     }
 }
 
+// Создаем хранилище Redux
 const store = Redux.createStore(counter);
+
+const count = document.querySelector('#count');
+const inBtn = document.querySelector('#increment');
+const deBtn = document.querySelector('#decrement');
+
+function render() {
+    count.innerHTML = store.getState();
+}
+
+store.subscribe(render);
+render();
+
+inBtn.addEventListener('click', function () {
+    store.dispatch({ type: 'INCREMENT' });
+});
+
+deBtn.addEventListener('click', function () {
+    store.dispatch({ type: 'DECREMENT' });
+});
