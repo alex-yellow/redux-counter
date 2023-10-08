7060f02dc600b80ce63c988aca5df00c4a9e3bd5
alex-yellow <hitys2000@gmail.com>
2023-10-08 14:05:06 +0300

    add redux.js

---
+++ redux.js
@@ -0,0 +1,13 @@
+function counter(state=0, action){
+    if(action.type == 'INCREMENT'){
+        return state + 1;
+    }
+    else if(action.type == 'DECREMENT'){
+        return state - 1;
+    }
+    else{
+        return state;
+    }
+}
+
+const store = Redux.createStore(counter);
